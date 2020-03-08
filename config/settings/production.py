7028d87d6d9715ef58e6969620513ebfcde147e4
William Boampong <wk4christ@gmail.com>
2020-03-08 13:39:32 +0000

    Use dj-database-url for heroku

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,18 +1,24 @@
 # settings/production.py
-
+import dj_database_url
 from .base import *
 print('In production')
 
 ALLOWED_HOSTS = ['*']
 DEBUG = True
+INSTALLED_APPS += ('whitenoise.runserver_nostatic',)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DJANGO_DB_NAME'),
-        'USER': os.getenv('DJANGO_DB_USER'),
-        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
-        'HOST': os.getenv('DJANGO_DB_HOST'),
-        'PORT': os.getenv('DJANGO_DB_PORT'),
-    }
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE += ('whitenoise.middleware.WhiteNoiseMiddleware',)
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] = dj_database_url.config()
+
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
+
+#location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+]
