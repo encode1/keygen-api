309e6a83124046ac34ebfa3573025c1427907dac
William Boampong <wk4christ@gmail.com>
2020-03-08 13:10:50 +0000

    Convert production db engine to postgresql

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,3 +5,14 @@ print('In production')
 
 ALLOWED_HOSTS = ['*']
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DJANGO_DB_NAME'),
+        'USER': os.getenv('DJANGO_DB_USER'),
+        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
+        'HOST': os.getenv('DJANGO_DB_HOST'),
+        'PORT': os.getenv('DJANGO_DB_PORT'),
+    }
+}
