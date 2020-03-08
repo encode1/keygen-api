4e4946aeda74f3f9f0d44fa0ad56f43647fe24db
William Boampong <wk4christ@gmail.com>
2020-03-08 02:30:46 +0000

    Move CORS setting to top

--- keygen/apiviews.py
+++ keygen/apiviews.py
@@ -1,7 +1,4 @@
 from rest_framework.generics import ListCreateAPIView, DestroyAPIView
-from django.shortcuts import get_object_or_404
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
 
 from .models import Key
 from .serializers import KeySerializer
@@ -16,6 +13,5 @@ class KeyDestroy(DestroyAPIView):
     queryset = Key.objects.all()
 
     def perform_destroy(self, instance):
-        print(isinstance(instance, Key))
         instance.deleted = True
         instance.save()
